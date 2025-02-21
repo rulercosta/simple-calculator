d22781bad06710ddb72f4565eda53f8ddc4c8b72
rulercosta <rulercosta.mail@gmail.com>
2025-02-21 14:01:02 +0530

    Add service worker implementation for caching and offline support

---
+++ sw.js
@@ -0,0 +1,76 @@
+const CACHE_NAME = 'calculator-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './static/styles.css',
+  './static/script.js',
+  './static/manifest.json',
+  './README.md',
+  './static/android-chrome-192x192.png',
+  './static/android-chrome-512x512.png',
+  './static/apple-touch-icon.png',
+  './static/favicon-32x32.png',
+  './static/favicon-16x16.png',
+  './static/favicon.ico'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+      .then(() => self.skipWaiting()) 
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(cachedResponse => {
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+        return fetch(event.request)
+          .then(response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            const responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          })
+          .catch(() => {
+            if (event.request.mode === 'navigate') {
+              return caches.match('/index.html');
+            }
+            return new Response('Network error happened', {
+              status: 408,
+              headers: { 'Content-Type': 'text/plain' },
+            });
+          });
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(cacheName => {
+            if (cacheName !== CACHE_NAME) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+      .then(() => self.clients.claim()) 
+  );
+});
